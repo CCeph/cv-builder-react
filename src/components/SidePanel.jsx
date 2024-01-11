7fb0f4f8d5a325e9dd76125c393e283e0a7b71b6
CCeph <yousefelsisyy@gmail.com>
2024-01-11 04:41:33 -0400

    Remove unnecessary title from side panel

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -4,7 +4,6 @@ import "../styles/SidePanel.css"
 export default function SidePanel() {
     return (
         <div className="side-panel">
-        <h1>I am the side panel</h1>
         <FormSection></FormSection>
         </div>
     )
