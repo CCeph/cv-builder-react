cb43c34b4118f0892654c632dcb45ed825c2496b
CCeph <yousefelsisyy@gmail.com>
2024-01-11 21:57:30 -0400

    Add all personal info input fields

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -8,6 +8,9 @@ export default function SidePanel() {
         <FormSection title="Personal Info">
             <Input labelTitle="Full Name: " inputName="full-name"></Input>
             <Input labelTitle="Email: " inputName="email" inputType="email"></Input>
+            <Input labelTitle="Phone Number: " inputName="phone-number"></Input>
+            <Input labelTitle="Address Line 1: " inputName="address1"></Input>
+            <Input labelTitle="Address Line 2: " inputName="address2"></Input>
         </FormSection>
         </div>
     )
