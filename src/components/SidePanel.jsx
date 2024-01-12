c77ca5f997e9313e8a8f6f1c2d7c341a439d1b2c
CCeph <yousefelsisyy@gmail.com>
2024-01-11 21:50:51 -0400

    Add email input

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -7,6 +7,7 @@ export default function SidePanel() {
         <div className="side-panel">
         <FormSection title={"Personal Info"}>
             <Input labelTitle={"Full Name: "} inputName={"full-name"}></Input>
+            <Input labelTitle={"Email: "} inputName={"email"} inputType="email"></Input>
         </FormSection>
         </div>
     )
