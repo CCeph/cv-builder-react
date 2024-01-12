cfda98d725dd7bb27bbd70d8ab651d3b7186d846
CCeph <yousefelsisyy@gmail.com>
2024-01-11 21:51:26 -0400

    Remove unnecessary curly braces

--- src/components/SidePanel.jsx
+++ src/components/SidePanel.jsx
@@ -5,9 +5,9 @@ import "../styles/SidePanel.css"
 export default function SidePanel() {
     return (
         <div className="side-panel">
-        <FormSection title={"Personal Info"}>
-            <Input labelTitle={"Full Name: "} inputName={"full-name"}></Input>
-            <Input labelTitle={"Email: "} inputName={"email"} inputType="email"></Input>
+        <FormSection title="Personal Info">
+            <Input labelTitle="Full Name: " inputName="full-name"></Input>
+            <Input labelTitle="Email: " inputName="email" inputType="email"></Input>
         </FormSection>
         </div>
     )
