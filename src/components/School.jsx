43450e368eb2b014edfd1ab6344d3cef07e8597a
CCeph <yousefelsisyy@gmail.com>
2024-01-14 05:11:05 -0400

    Add toggle for education subsections

--- src/components/School.jsx
+++ src/components/School.jsx
@@ -1,20 +1,30 @@
 import Input from "./Input";
 import "../styles/School.css"
+import { useState } from "react";
 
 export default function School({schoolID}) {
+    const [showDetails, setShowDetails] = useState(false);
+
+    function toggleDetails() {
+        setShowDetails(!showDetails);
+    }
+
     return (
         <div className="school">
-        <button className="detailsToggle">
+        <button className="detailsToggle" onClick={toggleDetails}>
             <h2>Name will go here</h2> <img src="src/assets/keyboard_arrow_down_FILL0_wght400_GRAD0_opsz24.svg"/>
         </button>
-        <hr />
-        <Input labelTitle="Institute Name: " inputName={`${schoolID}+institute-name`}></Input>
-        <Input labelTitle="City of Institute: " inputName={`${schoolID}+institute-city`}></Input>
-        <Input labelTitle="Province of Institute: (Optional)" inputName={`${schoolID}+institute-province`}></Input>
-        <Input labelTitle="Country of Institute: " inputName={`${schoolID}+institute-country`}></Input>
-        <Input labelTitle="Degree/Program: " inputName={`${schoolID}+program`}></Input>
-        <Input labelTitle="Start Date: " inputName={`${schoolID}+education-start-date`} inputType="date"></Input>
-        <Input labelTitle="End Date: " inputName={`${schoolID}+education-end-date`} inputType="date"></Input>
+        {showDetails && 
+                <div className="details">
+                <hr />
+                <Input labelTitle="Institute Name: " inputName={`${schoolID}+institute-name`}></Input>
+                <Input labelTitle="City of Institute: " inputName={`${schoolID}+institute-city`}></Input>
+                <Input labelTitle="Province of Institute: (Optional)" inputName={`${schoolID}+institute-province`}></Input>
+                <Input labelTitle="Country of Institute: " inputName={`${schoolID}+institute-country`}></Input>
+                <Input labelTitle="Degree/Program: " inputName={`${schoolID}+program`}></Input>
+                <Input labelTitle="Start Date: " inputName={`${schoolID}+education-start-date`} inputType="date"></Input>
+                <Input labelTitle="End Date: " inputName={`${schoolID}+education-end-date`} inputType="date"></Input>
+            </div>}
     </div>
     )
 }
