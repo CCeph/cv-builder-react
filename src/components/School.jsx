f04c420f538c576bb37a0602acad9a5b2dc347b3
CCeph <yousefelsisyy@gmail.com>
2024-01-14 04:58:12 -0400

    Add education subsection title

--- src/components/School.jsx
+++ src/components/School.jsx
@@ -4,6 +4,10 @@ import "../styles/School.css"
 export default function School({schoolID}) {
     return (
         <div className="school">
+        <button className="detailsToggle">
+            <h2>Name will go here</h2> <img src="src/assets/keyboard_arrow_down_FILL0_wght400_GRAD0_opsz24.svg"/>
+        </button>
+        <hr />
         <Input labelTitle="Institute Name: " inputName={`${schoolID}+institute-name`}></Input>
         <Input labelTitle="City of Institute: " inputName={`${schoolID}+institute-city`}></Input>
         <Input labelTitle="Province of Institute: (Optional)" inputName={`${schoolID}+institute-province`}></Input>
