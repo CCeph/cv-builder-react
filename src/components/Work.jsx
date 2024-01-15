23cec4befbaeebab8c71364a08ab3e3da09b9bce
CCeph <yousefelsisyy@gmail.com>
2024-01-14 22:06:32 -0400

    Add value/scriptNames to work inputs

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -32,29 +32,48 @@ export default function Work({
           <FlexInput
             labelTitle="Employer Name: "
             inputName="employer-name"
+            value={employerName}
+            scriptName="employerName"
+            /* 
+            valueHandler={updateSchoolInputs} */
           ></FlexInput>
           <FlexInput
             labelTitle="City of Employer: "
             inputName="employer-city"
+            value={city}
+            scriptName="city"
           ></FlexInput>
           <FlexInput
             labelTitle="Province of Employer: (Optional)"
             inputName="employer-province"
+            value={province}
+            scriptName="province"
           ></FlexInput>
           <FlexInput
             labelTitle="Country of Employer: "
             inputName="employer-country"
+            value={country}
+            scriptName="country"
           ></FlexInput>
-          <FlexInput labelTitle="Job Title: " inputName="job-title"></FlexInput>
+          <FlexInput
+            labelTitle="Job Title: "
+            inputName="job-title"
+            value={jobTitle}
+            scriptName="jobTitle"
+          ></FlexInput>
           <FlexInput
             labelTitle="Start Date: "
             inputName="job-start-date"
             inputType="date"
+            value={startDate}
+            scriptName="startDate"
           ></FlexInput>
           <FlexInput
             labelTitle="End Date: "
             inputName="job-end-date"
             inputType="date"
+            value={endDate}
+            scriptName="endDate"
           ></FlexInput>
           <button
             className="deleteSection"
