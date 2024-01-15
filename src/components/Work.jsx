a2be8cb266efba7d51da2e61936a37a4bba0e37e
CCeph <yousefelsisyy@gmail.com>
2024-01-14 21:42:41 -0400

    Style work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,5 +1,5 @@
 import FlexInput from "./FlexInput";
-import "../styles/School.css";
+import "../styles/Work.css";
 import { useState } from "react";
 
 export default function Work() {
@@ -10,40 +10,49 @@ export default function Work() {
   }
 
   return (
-    <div className="details">
-      <FlexInput
-        labelTitle="Employer Name: "
-        inputName="employer-name"
-      ></FlexInput>
-      <FlexInput
-        labelTitle="City of Employer: "
-        inputName="employer-city"
-      ></FlexInput>
-      <FlexInput
-        labelTitle="Province of Employer: (Optional)"
-        inputName="employer-province"
-      ></FlexInput>
-      <FlexInput
-        labelTitle="Country of Employer: "
-        inputName="employer-country"
-      ></FlexInput>
-      <FlexInput labelTitle="Job Title: " inputName="job-title"></FlexInput>
-      <FlexInput
-        labelTitle="Start Date: "
-        inputName="job-start-date"
-        inputType="date"
-      ></FlexInput>
-      <FlexInput
-        labelTitle="End Date: "
-        inputName="job-end-date"
-        inputType="date"
-      ></FlexInput>
-      <button
-        className="deleteSection"
-        onClick={() => deleteSchoolFromList(schoolID)}
-      >
-        Delete
+    <div className="school">
+      <button className="detailsToggle" onClick={toggleDetails}>
+        <h2>Work</h2>{" "}
+        <img src="src/assets/keyboard_arrow_down_FILL0_wght400_GRAD0_opsz24.svg" />
       </button>
+
+      {showDetails && (
+        <div className="details">
+          <FlexInput
+            labelTitle="Employer Name: "
+            inputName="employer-name"
+          ></FlexInput>
+          <FlexInput
+            labelTitle="City of Employer: "
+            inputName="employer-city"
+          ></FlexInput>
+          <FlexInput
+            labelTitle="Province of Employer: (Optional)"
+            inputName="employer-province"
+          ></FlexInput>
+          <FlexInput
+            labelTitle="Country of Employer: "
+            inputName="employer-country"
+          ></FlexInput>
+          <FlexInput labelTitle="Job Title: " inputName="job-title"></FlexInput>
+          <FlexInput
+            labelTitle="Start Date: "
+            inputName="job-start-date"
+            inputType="date"
+          ></FlexInput>
+          <FlexInput
+            labelTitle="End Date: "
+            inputName="job-end-date"
+            inputType="date"
+          ></FlexInput>
+          <button
+            className="deleteSection"
+            onClick={() => deleteSchoolFromList(schoolID)}
+          >
+            Delete
+          </button>
+        </div>
+      )}
     </div>
   );
 }
